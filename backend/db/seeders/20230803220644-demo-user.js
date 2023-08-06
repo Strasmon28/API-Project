54f4a1de19afc7202d9d83fa677d26cd9f7e120f
Shawn <svtrasmonte28@yahoo.com>
2023-08-06 16:45:00 -0500

    Revised seeder file

--- backend/db/seeders/20230803220644-demo-user.js
+++ backend/db/seeders/20230803220644-demo-user.js
@@ -1,25 +1,15 @@
 'use strict';
-/** @type {import('sequelize-cli').Migration} */
 
 const { User } = require('../models');
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 let options = {};
 if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;
+  options.schema = process.env.SCHEMA;  // define your schema in options object
 }
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
     await User.bulkCreate([
       {
         email: 'demo@user.io',
@@ -40,12 +30,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
     options.tableName = 'Users';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
@@ -53,3 +37,5 @@ module.exports = {
     }, {});
   }
 };
+
+// /** @type {import('sequelize-cli').Migration} */
