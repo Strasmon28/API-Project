b6f4065fe79732df62c97fff03b9586152e02cd8
Shawn <svtrasmonte28@yahoo.com>
2023-09-04 16:15:55 -0500

    Edited the home page

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { allSpots } from "../../store/spots";
+import noImage from "../GetOneSpot/OneSpotImages/na.jpg";
 import "./Home.css";
 
 function Home() {
@@ -24,18 +25,25 @@ function Home() {
     <div className="border">
       {spots.map((spot) => (
         <NavLink to={`/spotDetail/${spot.id}`}>
-          <div key={spot.id}>
+          <div className="oneSpot" key={spot.id}>
             <div className="imageHover">
-              {spot.previewImage}
               <span className="tooltipText">{spot.name}</span>
+              {spot.previewImage ? (
+                <p className="placeholder">{spot.previewImage}</p>
+              ) : (
+                <img class="spotImage" src={noImage} alt="preview" />
+              )}
             </div>
             <div className="section2">
-            <div className="leftInfo">
-              {spot.city} {spot.state} ${spot.price} night
-            </div>
-            {spot.avgRating ? (
-              <i className="fa-solid fa-star">{spot.avgRating}</i>
-            ):<i className="fa-solid fa-star">New</i>}
+              <div className="leftInfo">
+                <p>{spot.city} {spot.state}</p>
+                <p>${spot.price} night</p>
+              </div>
+              {spot.avgRating ? (
+                <i className="fa-solid fa-star rating">{spot.avgRating}</i>
+              ) : (
+                <i className="fa-solid fa-star rating">New</i>
+              )}
             </div>
           </div>
         </NavLink>
